891e948694037fda0f3ee0c9172a7cf68509bc23
YuraKIM <155035987+yuraKIMyura@users.noreply.github.com>
2024-05-08 13:12:14 +0900

    Update ProjectSecurityConfig.java
    DB변경 request 차단하는 CSRF 기능 해제했습니다.
    그리고 다들 controller에서 URL 지정한 뒤에 Security Config 내부 requestMatchers에 URL 추가해주는 것 꼭 잊지 마세요!

--- src/main/java/com/team1/mohaji/config/ProjectSecurityConfig.java
+++ src/main/java/com/team1/mohaji/config/ProjectSecurityConfig.java
@@ -11,7 +11,9 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
+        http
+                .csrf(csrf -> csrf.disable()) //CSRF protection: DB변동이 발생하는 request는 무조건 차단시켜버리는 기능. DB작업하려고 해제.
+                .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/authenticated").authenticated()
                 .requestMatchers("/unauthenticated").permitAll())
                 .formLogin(Customizer.withDefaults())
