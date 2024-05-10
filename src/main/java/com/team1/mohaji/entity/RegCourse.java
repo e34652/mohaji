3e628f3804d71d952afadf74d156f951e4895404
e34652 <155132260+e34652@users.noreply.github.com>
2024-05-10 14:07:19 +0900

    merged by jjui

--- src/main/java/com/team1/mohaji/entity/RegCourse.java
+++ src/main/java/com/team1/mohaji/entity/RegCourse.java
@@ -1,25 +1,25 @@
-package com.team1.mohaji.entity;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "reg_course")
-public class RegCourse {
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "stu_id", nullable = false)
-    private Student student;
-
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "sub_id", nullable = false)
-    private Subject subject;
-
-    @Column(name = "rc_stat")
-    private String rcStat;
-
-    @Column(name = "rc_progress", nullable = false)
-    private double rcProgress;
-
-    // Constructors, getters, and setters
-}
+//package com.team1.mohaji.entity;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name = "reg_course")
+//public class RegCourse {
+//    @Id
+//    @ManyToOne
+//    @JoinColumn(name = "stu_id", nullable = false)
+//    private Student student;
+//
+//    @Id
+//    @ManyToOne
+//    @JoinColumn(name = "sub_id", nullable = false)
+//    private Subject subject;
+//
+//    @Column(name = "rc_stat")
+//    private String rcStat;
+//
+//    @Column(name = "rc_progress", nullable = false)
+//    private double rcProgress;
+//
+//    // Constructors, getters, and setters
+//}
