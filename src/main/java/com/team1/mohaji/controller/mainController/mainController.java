b3b590694f6f74f34c741f5a0534c6ae36e5503d
jjuiru <shj00035@naver.com>
2024-05-10 21:38:59 +0900

    commit
    cm

---
+++ src/main/java/com/team1/mohaji/controller/mainController/mainController.java
@@ -0,0 +1,19 @@
+package com.team1.mohaji.controller.mainController;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class mainController {
+
+    @GetMapping("/")
+    public  String main1()
+    {return "/view/main";}
+
+    @GetMapping("/main")
+    public  String main2(){
+    return "/view/main";
+}
+    }
