16f6920184e3d2d5d13d522ba6c4d72a3013e890
e34652 <155132260+e34652@users.noreply.github.com>
2024-05-13 01:58:16 +0900

    config 수정

--- src/main/java/com/team1/mohaji/config/SecurityConfig.java
+++ src/main/java/com/team1/mohaji/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) //CSRF protection: DB변동이 발생하는 request는 무조건 차단시켜버리는 기능. DB작업하려고 해제.
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/static/**", "/js/**", "/css/**", "/img/**", "/json/**", "/resources/**", "/error").permitAll()
+                        .requestMatchers("/js/**", "/css/**", "/img/**", "/json/**").permitAll()
                         .requestMatchers("/", "/main").permitAll()
                         .requestMatchers("/register").permitAll()
                         .anyRequest().authenticated())
