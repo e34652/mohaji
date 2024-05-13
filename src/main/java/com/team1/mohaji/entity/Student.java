df436ed39ffce3f6620972292f81232ddc8ba726
YuraKIM <155035987+yuraKIMyura@users.noreply.github.com>
2024-05-13 21:42:26 +0900

    Student Entity updated
    FetchType.LAZY > FetchType.EAGER

--- src/main/java/com/team1/mohaji/entity/Student.java
+++ src/main/java/com/team1/mohaji/entity/Student.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @Table(name="students")
 public class Student extends BaseEntity {
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member_id", referencedColumnName = "member_id", nullable = false, unique=true)
     private Member member;
 
