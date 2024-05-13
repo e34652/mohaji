0748602c14e1555e008ce90784e41963a7677de9
e34652 <155132260+e34652@users.noreply.github.com>
2024-05-13 16:28:40 +0900

    GridTest

--- src/main/java/com/team1/mohaji/config/SecurityConfig.java
+++ src/main/java/com/team1/mohaji/config/SecurityConfig.java
@@ -17,10 +17,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())//CSRF protection: DB변동이 발생하는 request는 무조건 차단시켜버리는 기능. DB작업하려고 해제.
                 .authorizeHttpRequests((requests) -> requests // HTTP 요청을 인증 및 권한 부여합니다.
-                        .requestMatchers("/css/**", "/img/**" ,"/js/**").permitAll() // "모든 외부링크 적용 허용"
-                        .requestMatchers("/", "/main").permitAll() // "/"와 "/main" 경로에 대한 요청은 모두 허용합니다.
-                        .requestMatchers("/register").permitAll() // "/register" 경로에 대한 요청은 모두 허용합니다.
-                        .anyRequest().authenticated())// 그 외의 모든 요청은 인증이 필요합니다.
+//                        .requestMatchers("/css/**", "/img/**" ,"/js/**").permitAll() // "모든 외부링크 적용 허용"
+//                        .requestMatchers("/", "/main", "/test/**", "/test/getList").permitAll() // "/"와 "/main" 경로에 대한 요청은 모두 허용합니다.
+//                        .requestMatchers("/register").permitAll() // "/register" 경로에 대한 요청은 모두 허용합니다.
+                        .anyRequest().permitAll())// 그 외의 모든 요청은 인증이 필요합니다.
                 .formLogin(loginConfigurer -> loginConfigurer // 폼 로그인을 구성합니다.
                         .defaultSuccessUrl("/main",true) // 로그인 성공 시 기본 URL("/main")로 리다이렉트합니다.
                         .failureUrl("/login?error=true") // 로그인 실패 시 "/login?error=true" URL로 리다이렉트합니다.
