220950a1c8a5beca12861f3a0da2d6f7abf1c0fd
e34652 <155132260+e34652@users.noreply.github.com>
2024-05-14 18:09:13 +0900

    dummy

---
+++ src/main/resources/db/Tak/dummy.sql
@@ -0,0 +1,16 @@
+더미코드
+
+video
+insert into video values(0,"파이썬 기초 강의", 540,"파이썬 기초 강의 [1강.파이썬 시작하기]", "UMfCZMuZoRk");
+
+department
+insert into department values(0,"여기는 컴공과","컴퓨터공학과","dc0101");
+
+subject
+insert into subject values(0,"THREE", "파이썬을 공부해보자","파이썬 기초", 30, 1, 1,"전공선택");
+
+criteria_common
+insert into criteria_common values("dc01","학과공통기준");
+
+criteria_detail
+insert into criteria_detail values("dc0101","모든 학과 공통","졸업요구학점", 130, null, null, null, null, "dc01");
