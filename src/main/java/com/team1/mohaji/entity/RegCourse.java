b0edb452c3ed3183653d49606ac343db7bb5a11e
jjuiru <155119093+jjuiru@users.noreply.github.com>
2024-05-16 03:06:29 +0900

    cm
    cm

--- src/main/java/com/team1/mohaji/entity/RegCourse.java
+++ src/main/java/com/team1/mohaji/entity/RegCourse.java
@@ -2,7 +2,6 @@ package com.team1.mohaji.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -23,12 +22,13 @@ public class RegCourse {
     @Id
     @ManyToOne(optional = true)
     @JoinColumn(name = "member_id", referencedColumnName = "member_id")
-    private Student member;
+    private Student student;
 
     @Id
     @ManyToOne(optional = true)
     @JoinColumn(name = "sub_id" , referencedColumnName = "sub_id")
     private Subject subject;
+
 
     @Column(name = "cc_id", length = 30) // 공통기준 테이블 /select
     private String ccId;
@@ -57,4 +57,6 @@ public class RegCourse {
         Completed, //이수
         Cancelled //취소
     }
+
 }
+
