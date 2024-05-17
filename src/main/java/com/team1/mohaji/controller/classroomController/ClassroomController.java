93c2a447dda99426191715f51395f80a9add7791
e34652 <155132260+e34652@users.noreply.github.com>
2024-05-17 21:35:18 +0900

    doing viewer page

--- src/main/java/com/team1/mohaji/controller/classroomController/ClassroomController.java
+++ src/main/java/com/team1/mohaji/controller/classroomController/ClassroomController.java
@@ -2,15 +2,17 @@ package com.team1.mohaji.controller.classroomController;
 
 
 
-import com.team1.mohaji.config.CustomUserDetails;
-import com.team1.mohaji.dto.myPage.RegListDto;
-import com.team1.mohaji.service.myPage.imple.RegListServiceImple;
+import com.team1.mohaji.dto.Classroom.ClassroomHomeDto;
+import com.team1.mohaji.model.model;
+import com.team1.mohaji.service.Classroom.imple.ClassroomHomeHomeServiceImple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,18 +22,26 @@ import java.util.List;
 public class ClassroomController {
 
     @Autowired
-    private RegListServiceImple regListServiceImple;
+    private ClassroomHomeHomeServiceImple classroomHomeServiceImple;
 
     @GetMapping("/home")
-        public String myList (@AuthenticationPrincipal CustomUserDetails userDetails, Model model){
+        public String myList (
+                @AuthenticationPrincipal UserDetails userDetails,
+                @RequestParam int subId,
+                @RequestParam int memberId,
+                Model model){
             if (userDetails != null) {
-                int memberId = userDetails.getMemberId();
-                List<RegListDto> regList = regListServiceImple.regListInProgress(memberId);
-                model.addAttribute("regList", regList);
-                System.out.println(regList);
-                return "/view/myPage/regList";
+                List<ClassroomHomeDto> home = classroomHomeServiceImple.sessionListInProgress(subId,memberId);
+                model.addAttribute("home", home);
+                System.out.println(home);
+                return "/view/classroom/classroomHome";
             } else {
-                throw new NullPointerException("로그인 유저 정보가 없습니다");
+                return "redirect:/login";
             }
         }
+    @GetMapping("/viewer")
+    public String myList (){
+            return "/view/classroom/viewer";
+        }
     }
+
--- src/main/java/com/team1/mohaji/controller/myPageController/MyPageController.java
+++ src/main/java/com/team1/mohaji/controller/myPageController/MyPageController.java
@@ -10,16 +10,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 
 @Controller
 @RequestMapping("/myPage")
-public class courseListController {
+public class MyPageController {
 
     @Autowired
 
