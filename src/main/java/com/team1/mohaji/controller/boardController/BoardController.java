a971c825408acb35620f0e851c939a8003e4aac9
e34652 <155132260+e34652@users.noreply.github.com>
2024-05-29 11:21:53 +0900

    커뮤니티 로그아웃 회원이름 출력

--- src/main/java/com/team1/mohaji/controller/boardController/BoardController.java
+++ src/main/java/com/team1/mohaji/controller/boardController/BoardController.java
@@ -1,5 +1,6 @@
 package com.team1.mohaji.controller.boardController;
 
+import com.team1.mohaji.config.CustomUserDetails;
 import com.team1.mohaji.dto.PostDto;
 import com.team1.mohaji.entity.Board;
 import com.team1.mohaji.entity.Post;
@@ -7,15 +8,23 @@ import com.team1.mohaji.service.board.BoardService;
 import com.team1.mohaji.service.board.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class BoardController {
+
+    @ModelAttribute
+    public void addAttributes(Model model, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        System.out.println(customUserDetails.getName());
+        model.addAttribute("name", customUserDetails.getName());
+    }
 
     @Autowired
     private BoardService boardService;
